fbcdbea4bf10035f2682ca15215e087d598bac86
ccckmit <ccckmit@gmail.com>
2018-10-03 09:45:53 +0800

    modify body-parsing add form submit

--- example/03-koa/03-body-parsing/app.js
+++ example/03-koa/03-body-parsing/app.js
@@ -1,8 +1,11 @@
 
 const Koa = require('koa');
 const koaBody = require('koa-body');
+const serve = require('koa-static');
 
 const app = module.exports = new Koa();
+
+app.use(serve('.'));
 
 app.use(koaBody({
   jsonLimit: '1kb'
@@ -13,6 +16,7 @@ app.use(koaBody({
 // and application/x-www-form-urlencoded
 
 app.use(async function(ctx) {
+  if (ctx.url !== '/uppercase') return
   const body = ctx.request.body;
   if (!body.name) ctx.throw(400, '.name required');
   ctx.body = { name: body.name.toUpperCase() };
