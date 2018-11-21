fd88af5cd4fbd8ce8aa2b26812613a8ad0528735
ccckmit <ccckmit@gmail.com>
2018-11-21 09:44:32 +0800

    fix bug in ajaxTest

--- example/04-classic/03-blogMvc/test.js
+++ example/04-classic/03-blogMvc/test.js
@@ -8,20 +8,20 @@ describe('簡易網誌系統', function () {
     server.close()
   })
 
-  describe('GET /', function () { // 路徑 GET /
+  describe('GET /', function () {
     it('內文標題應該為《貼文列表》，而且只有 0 則貼文', function (done) {
       request.get('/').expect(200, function (err, res) {
         if (err) return done(err)
 
-        expect(res.header['content-type']).to.include('html') // 根目錄是個 html 文件
-        expect(res.text).to.include('<title>貼文列表</title>') // 內文標題為 Posts
+        expect(res.header['content-type']).to.include('html')
+        expect(res.text).to.include('<title>貼文列表</title>')
         expect(res.text).to.include('<p>您總共有 <strong>0</strong> 則貼文!</p>')
         done()
       })
     })
   })
 
-  describe('POST /post', function () { // 路徑 POST /post/new
+  describe('POST /post', function () {
     it('應該會創建新貼文，然後轉址到根目錄 /', function (done) {
       request
         .post('/post')
@@ -29,7 +29,7 @@ describe('簡易網誌系統', function () {
         .end(function (err, res) {
           if (err) return done(err)
 
-          expect(res.header.location).to.equal('/') // 路徑 / => 根目錄是個 html 文件
+          expect(res.header.location).to.equal('/')
           done()
         })
     })
